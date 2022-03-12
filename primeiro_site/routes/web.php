3b59cb196791401b3cf510306ad0eb318c26405c
LUIZ FERNANDO ALBERTIN BONO MILAN <luiz.fabmilan@senacsp.edu.br>
2022-03-11 22:23:19 -0300

    Final da aula

--- primeiro_site/routes/web.php
+++ primeiro_site/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/avisos', function (){
+    return view('avisos',[  'nome' => 'Albertin', 
+                            'mostrar' => true,
+                            'avisos' => [   ['id' => 1, 'aviso' => 'Mussum Ipsum, cacilds vidis litro abertis'],
+                                            ['id' => 2, 'aviso' => 'Em pé sem cair, deitado sem dormir'],
+                                            ['id' => 3, 'aviso' => 'A ordem dos tratores não altera o pão duris']]]);
+});
